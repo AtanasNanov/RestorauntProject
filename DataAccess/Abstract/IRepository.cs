f8ff1c90b215ebf28e5ff831e06a411f96defd03
Nikola Valkov <bulgarianmapper64@gmail.com>
2022-10-24 17:36:58 +0300

    Implemented IRepository interface defining CRUD operations

---
+++ DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IRepository : IDisposable
+    {
+        //Lipsva class Employee taka che metodite ne rabotqt za sega
+        IEnumerable<Employee> GetEmployees(); 
+        Employee GetEmployeeByID(int employeeId);
+        void InsertEmplyee(Employee employee);
+        void DeleteEmployee(int employeeId);
+        void UpdateEmployee(Employee employee);
+        void Save();
+
+    }
+}
